f4b59e22f9033251060e5ebf511b88f3415a69c1
szy <szy@noreply.com>
2022-11-23 17:34:22 +0800

    init

---
+++ nacos_registry/nacos_web.go
@@ -0,0 +1,20 @@
+package nacos_registry
+
+import (
+	"github.com/micro/go-micro/v2/registry"
+	"github.com/micro/go-micro/v2/web"
+)
+
+func WebRegistry(r registry.Registry) web.Option {
+	return func(o *web.Options) {
+		o.Registry = r
+		if o.Metadata == nil {
+			o.Metadata = make(map[string]string)
+		}
+		o.Metadata["broker"] = "http"
+		o.Metadata["http"] = "http"
+		o.Metadata["registry"] = "nacos"
+		o.Metadata["server"] = "http"
+		o.Metadata["transport"] = "http"
+	}
+}
